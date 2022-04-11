7d5a90a02f9671fef4f971ad005ee2e24c4d7d72
Joshua Diaz <jad24096@gmail.com>
2022-04-10 22:09:52 -0400

    attempted adding search history.

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,73 @@
-// global variables
-const searchHistory = [];
-const weatherApiUrl = "https://api.openweathermap.org/";
-const apikey = "f9045589f9bcbeef853eb7e06f209ddb";
+// // global variables
+// const searchHistory = [];
+// const weatherApiUrl = "https://api.openweathermap.org/";
+// const apikey = "f9045589f9bcbeef853eb7e06f209ddb";
+// var cityName
 
-// DOM Elments References
-// #search form, #search-input, #today, #forecast, #history
-let searchForm = document.querySelector(#search-form);
-let searchInput = 
-let todayBox =
-let forecastBox =
-let searchDisplayBox =
+// // DOM Elments References
+// // let searchForm = document.querySelector(#search-form);
+// // var searchBtn = document.querySelector("#search");
+// // let todayBox =
+// // let forecastBox =
+// // let searchDisplayBox =
 
-// add timezone for day.js
-dayjs.extend(window.dayjs_plgin_utc);
-dayjs.extend(window.dayjs_plugin_timezone);
+// searchBtn.addEventListener("click", ); 
 
-// function to display search history 
-// function displaySearchHistory() {
-//      searchDisplayBox.innerhtml = "",
+// // add timezone for day.js
+// dayjs.extend(window.dayjs_plgin_utc);
+// dayjs.extend(window.dayjs_plugin_timezone);
+
+// // function to display search history 
+// // function displaySearchHistory() {
+// //      searchDisplayBox.innerhtml = "",
+// // }
+
+// var weatherDataApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=5&appid={f9045589f9bcbeef853eb7e06f209ddb}"
+
+// function weatherData(weatherApi) {
+//     fetch(weatherApi)
+//     .then(fucntion(res) {
+//         return res.json();
+//     })
+//     .then(function(data) {
+//         console.log(data)
+//     })
+// }
+// weatherData(weatherApiUrl);
+
+// // make a request to the one call and show them how to make api key
+// // fetch coordinates
+// // fetch weather
+// // function renderItems(city, data) {
+
+// // }
+
+// // 1. geocoding API for lat and long
+// function cityInput(location) {
+//     let { lat } = location;
+//     let { long } = location;
+//     let cityName = location.name;
+//     let apiurl = ${weatherApiUrl};
+
+
+// }
+// var searchBtn = document.querySelector("#search");
+
+// searchBtn.addEventListener("click", historyAdd); 
+
+// function historyAdd() {
+//     // $(document).ready(function() {
+//     $("#search").on("click", function() {
+//     var history = document.createElement("h6");
+//     // var cityInput = $(this).siblings(".information").val();
+//     var textnode = document.createTextNode("");
+//     // var time = $(this).parent().attr("id");
+//     history.appendChild(textnode);
+//     localStorage.setItem(history);
+//     })
 // }
 
-var weatherDataApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}"
 
-function weatherData(weatherApi) {
-    fetch(weatherApi)
-    .then(fucntion(Res) {
-        return res.json()
-    })
-    .then(function(data) {
-        console.log(data)
-    })
-}
-weatherData(weatherApiUrl);
+// historyAdd();
 
-// make a request to the one call and show them how to make api key
-// fetch coordinates
-// fetch weather
-// function renderItems(city, data) {
-
-// }
-
-// 1. geocoding API for lat and long
-function fetchWeather(location) {
-    let { lat } = location;
-    let { long } = location;
-    let cityName = location.name;
-    let apiurl = ${weatherApiUrl}...;
-
-}
+// $(".history").val(localStorage.getItem("history"));
---
+++ assets/script2.js
@@ -0,0 +1,23 @@
+const searchHistory = [];
+const weatherApiUrl = "https://api.openweathermap.org/";
+const apikey = "f9045589f9bcbeef853eb7e06f209ddb";
+
+let searchForm = document.querySelector("#search-form");
+var searchBtn = document.getElementById("search");
+// let todayBox =
+// let forecastBox =
+let history = document.querySelector(".history");
+let searchBar = document.querySelector("#searchbar"); 
+
+
+function test() {
+    if (searchBtn) {
+        searchHistory.push();
+       } 
+       console.log(searchHistory);
+}
+
+
+
+
+searchBtn.addEventListener("click", test);
