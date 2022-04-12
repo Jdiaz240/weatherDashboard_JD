d72fd1758cf91e3d5e2ce8b24f5f4833971a34a4
Joshua Diaz <jad24096@gmail.com>
2022-04-11 23:05:21 -0400

    completed fetch for both Api's.

--- assets/script2.js
+++ assets/script2.js
@@ -6,15 +6,31 @@ let searchForm = document.querySelector("#search-form");
 var searchBtn = document.getElementById("search");
 // let todayBox =
 // let forecastBox =
-let history = document.querySelector(".history");
+let history = document.getElementById(".history");
 let searchBar = document.querySelector("#searchbar"); 
 
 
 function test() {
-   
+fetch("http://api.openweathermap.org/data/2.5/onecall?lon=23&lat=56&appid=f9045589f9bcbeef853eb7e06f209ddb", {
+  "method": "GET",
+  "headers": {}
+})
+.then(response => {
+  console.log(response);
+})
+.catch(err => {
+  console.error(err);
+});
+
+fetch("http://api.openweathermap.org/geo/1.0/direct?q=orlando&appid=f9045589f9bcbeef853eb7e06f209ddb", {
+  "method": "GET",
+  "headers": {}
+})
+.then(response => {
+  console.log(response);
+})
+.catch(err => {
+  console.error(err);
+});
 }
-
-
-
-
-searchBtn.addEventListener("click", test);
+test();
